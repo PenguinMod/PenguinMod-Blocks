545c0bd307216ca50672b90306e2881a250252a3
SharkPool <139097378+SharkPool-SP@users.noreply.github.com>
2025-08-28 10:04:59 -0700

    Update field_expandable_remove.js

--- core/field_expandable_remove.js
+++ core/field_expandable_remove.js
@@ -72,6 +72,8 @@ Blockly.FieldExpandableRemove.prototype.init = function() {
   }
   Blockly.FieldExpandableRemove.superClass_.init.call(this);
 
+  this.size_.width = 32;
+  this.EDITABLE = false;
   this.boxGroup_ = Blockly.utils.createSvgElement('g', {}, null);
   this.box_ = Blockly.utils.createSvgElement('rect',
     {
@@ -89,9 +91,9 @@ Blockly.FieldExpandableRemove.prototype.init = function() {
   );
   this.icon_ = Blockly.utils.createSvgElement('image',
     {
-      'x': 2.5,
+      'x': 5,
       'y': 5,
-      'width': this.size_.height / 1.5,
+      'width': this.size_.width / 1.5,
       'height': this.size_.height / 1.5,
       'href': removeIcon,
     },
@@ -99,7 +101,6 @@ Blockly.FieldExpandableRemove.prototype.init = function() {
   );
   this.fieldGroup_.insertBefore(this.boxGroup_, this.textElement_);
 };
-
 
 /**
  * Returns an empty string.
