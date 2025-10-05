8614aebb0c38c3b34e5d20deeda506e3d58c2953
DogeisCut <23178844+DogeisCut@users.noreply.github.com>
2025-10-04 20:54:17 -0600

    make the if return not look weird

--- blocks_vertical/control.js
+++ blocks_vertical/control.js
@@ -1120,7 +1120,8 @@ Blockly.Blocks["control_if_return_else_return"] = {
       ],
       "output": null,
       "category": Blockly.Categories.control,
-      "extensions": ["colours_control"]
+      "extensions": ["colours_control"],
+      "outputShape": Blockly.OUTPUT_SHAPE_ROUND
     });
   }
 };
