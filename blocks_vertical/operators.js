3e2c0dc94ced6c06084769b3ca0a2506c6ac3547
SharkPool <139097378+SharkPool-SP@users.noreply.github.com>
2025-11-14 00:00:46 -0800

    Update operators.js

--- blocks_vertical/operators.js
+++ blocks_vertical/operators.js
@@ -1455,40 +1455,6 @@ Blockly.Blocks["operator_toUpperLowerCase"] = {
   }
 };
 
-Blockly.Blocks["operator_javascript_output"] = {
-  init: function () {
-    this.jsonInit({
-      "inputsInline": true,
-      "message0": "javascript %1",
-      "args0": [
-        {
-          "type": "input_value",
-          "name": "JS"
-        }
-      ],
-      "category": Blockly.Categories.operators,
-      "extensions": ["colours_operators", "output_string"]
-    });
-  }
-};
-
-Blockly.Blocks["operator_javascript_boolean"] = {
-  init: function () {
-    this.jsonInit({
-      "inputsInline": true,
-      "message0": "javascript %1",
-      "args0": [
-        {
-          "type": "input_value",
-          "name": "JS"
-        }
-      ],
-      "category": Blockly.Categories.operators,
-      "extensions": ["colours_operators", "output_boolean"]
-    });
-  }
-};
-
 Blockly.Blocks['operator_checkboxBoolean'] = {
   init: function() {
     this.jsonInit({
